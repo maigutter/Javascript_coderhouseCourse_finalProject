3b317ecd0b44f01ddc7514d936ef74c97017828f
maigutter <maigutter@gmail.com>
2023-02-21 23:07:23 -0300

    complejizo la tienda con opciones de administradore para editar cursos en venta, agregar cursos en venta y quitar cursos en venta

--- tienda.js
+++ tienda.js
@@ -13,6 +13,81 @@ const cursoTres = new CursoVenta("Inglés Avanzado", 5000, "./assets/logo4.jpeg"
 
 const arrayCursosVenta = [cursoUno, cursoDos, cursoTres];
 
+
+function menu() {
+    alert("Bienvenide a la Tienda Virtual!");
+    let opcion = parseInt(prompt("Elegí lo que querés hacer: 1)Acceder a la tienda! Quiero comprar. 2) Opción de admin: Agregar un curso a la tienda 3) Opción de admin: Sacar un curso de la tienda 4) Opción de Admin - Modificar un Curso 5) Salir"));
+    return opcion;
+}
+
+
+function bienvenida() {
+    alert("Bienvenide a la Tienda!");
+}
+
+function agregarCurso() {
+    let nombre = prompt("Ingrese el nombre del curso: ");
+    let precio = parseInt(prompt("Ingrese el precio del curso: "));
+    let img = "./assets/logo4.jpeg";
+    let descripcion = prompt("Ingrese la descripción del curso: ");
+    let cursoVenta = new CursoVenta(nombre, precio, img, descripcion);
+    arrayCursosVenta.push(cursoVenta);
+    console.log(arrayCursosVenta);
+}
+
+
+function quitarCurso() {
+    let nombre = prompt("Ingrese el nombre del curso:  ");
+    let cursoVenta = arrayCursosVenta.find(cursoVenta => cursoVenta.nombre === nombre);
+    let indice = arrayCursosVenta.indexOf(cursoVenta);
+    arrayCursosVenta.splice(indice, 1);
+    console.log(arrayCursosVenta);
+}
+
+
+function modificacionCurso() {
+    let nombreBuscado = prompt("Ingrese el nombre del curso:  ");
+    let cursoVenta = arrayCursosVenta.find(cursoVenta => cursoVenta.nombre === nombreBuscado);
+    let indice = arrayCursosVenta.indexOf(cursoVenta);
+
+    let nombre = prompt("Ingrese el nombre del curso: ");
+    let precio = parseInt(prompt("Ingrese el precio del cliente:  "));
+    let img = "./assets/logo4.jpeg";
+    let descripcion = prompt("Ingrese la descripción del curso: ");
+    let cursoModificado = new Cliente(nombre, precio, img, descripcion);
+
+    arrayCursosVenta.splice(indice, 1, cursoModificado);
+    console.log(arrayCursosVenta);
+}
+
+function salir() {
+    alert("Ojalá vuelvas pronto!");
+}
+
+
+let opcion = menu();
+
+switch(opcion) {
+    case 1: 
+        bienvenida();
+        break;
+    case 2:
+        agregarCurso();
+        break;
+    case 3: 
+        quitarCurso();
+        break;
+    case 4: 
+        modificacionCurso();
+        break;
+    case 5: 
+        salir();
+        break;
+    default:
+        alert("Lo sentimos! Esta no es una opción! Recargá la página y volvé a intentar :)");
+        break;
+}
+
 const contenedorTienda = document.getElementById("contenedorTienda");
 
 arrayCursosVenta.forEach( cursoVenta => {
@@ -26,3 +101,5 @@ arrayCursosVenta.forEach( cursoVenta => {
                     `;
     contenedorTienda.appendChild(div);
 })
+
+
